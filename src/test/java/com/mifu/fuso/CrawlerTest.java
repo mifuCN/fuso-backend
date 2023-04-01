45510bf3da8f4d559ae712c664cae189dfc2b124
mifuRD <1755786251@qq.com>
2023-04-01 21:30:34 +0800

    feat: ✨ 后端初始化
    编写爬虫的测试用例,并写入数据库

---
+++ src/test/java/com/mifu/fuso/CrawlerTest.java
@@ -0,0 +1,67 @@
+package com.mifu.fuso;
+
+import cn.hutool.http.HttpRequest;
+import cn.hutool.json.JSONArray;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import com.mifu.fuso.model.entity.Post;
+import com.mifu.fuso.service.PostService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 测试hutool的http客户端
+ * @Author mifuCN
+ * @Date 2023-04-01 20:23
+ **/
+@SpringBootTest
+public class CrawlerTest {
+    @Resource
+    private PostService postService;
+
+    @Test
+    void testFetchPassage() {
+        // 1.获取数据
+        String json = "{\n" +
+                "  \"current\": 1,\n" +
+                "  \"pageSize\": 8,\n" +
+                "  \"sortField\": \"thumbNum\",\n" +
+                "  \"sortOrder\": \"descend\",\n" +
+                "  \"category\": \"文章\",\n" +
+                "  \"reviewStatus\": 1\n" +
+                "}";
+        String url = "https://www.code-nav.cn/api/post/search/page/vo";
+        String result = HttpRequest.post(url)
+                .body(json)
+                .execute().body();
+        System.out.println(result);
+
+        // 2.获取到的 json 数据转为对象
+        Map<String, Object> map = JSONUtil.toBean(result, Map.class);
+        JSONObject data = (JSONObject) map.get("data");
+        JSONArray records = (JSONArray) data.get("records");
+        List<Post> postList = new ArrayList<>();
+        for (Object record : records) {
+            JSONObject tempRecord = (JSONObject) record;
+            Post post = new Post();
+            post.setTitle(tempRecord.getStr("title"));
+            post.setContent(tempRecord.getStr("content"));
+            JSONArray tags = (JSONArray) tempRecord.get("tags");
+            List<String> tagList = tags.toList(String.class);
+            post.setTags(JSONUtil.toJsonStr(tagList));
+            post.setUserId(1L);
+            postList.add(post);
+        }
+        System.out.println(postList);
+
+        // 3.数据入库(使用MP封装好的方法)
+        boolean flag = postService.saveBatch(postList);
+        Assertions.assertTrue(flag);
+    }
+}
